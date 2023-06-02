ca64846e1c6b1e116b18b476b87564cb7c2f59a5
Cristopher Gomez <sonrisita011@hotmail.com>
2023-06-02 13:29:32 -0500

    agregado recordar credenciales

--- app/src/main/java/com/example/actividades/MainActivity.java
+++ app/src/main/java/com/example/actividades/MainActivity.java
@@ -2,31 +2,61 @@ package com.example.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_PERMISSION_CODE = 1;
     private Toolbar toolbar;
     private EditText txt_usuario;
     private EditText txt_password;
-
+    private CheckBox chkRecordarCredenciales;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Grupo 6");
 
+        txt_usuario = findViewById(R.id.txt_usuario);
+        txt_password = findViewById(R.id.txt_password);
+
+        chkRecordarCredenciales = findViewById(R.id.checkBox);
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        if (sharedPreferences.contains("usuario") && sharedPreferences.contains("password")) {
+            String nombreUsuario = sharedPreferences.getString("usuario", "");
+            String clave = sharedPreferences.getString("password", "");
+            // Aquí puedes realizar la lógica para iniciar sesión automáticamente con los datos guardados
+            // y navegar a la pantalla principal de tu proyecto
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+        }
         Button button_aceptar = findViewById(R.id.button_aceptar);
         button_aceptar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,11 +95,24 @@ public class MainActivity extends AppCompatActivity {
         if (usuarioIngresado.equals(usuarioValido) && passwordIngresado.equals(passwordValido)) {
             Toast.makeText(this, "Credenciales correctas", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            if (chkRecordarCredenciales.isChecked()) {
+                // Guardar la información en SharedPreferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("usuario", usuarioIngresado);
+                editor.putString("password", passwordIngresado);
+                editor.apply();
+                guardarRegistroEnArchivo(usuarioIngresado,passwordIngresado);
+            }
             startActivity(intent);
         } else {
             Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void guardarRegistroEnArchivo(String usuario, String password) {
+        Toast.makeText(this, usuario, Toast.LENGTH_SHORT).show();
+    }
+
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater menu_barra = getMenuInflater();
         menu_barra.inflate(R.menu.menu, menu);
@@ -88,4 +131,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/com/example/actividades/MainActivity2.java
+++ app/src/main/java/com/example/actividades/MainActivity2.java
@@ -2,20 +2,24 @@ package com.example.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Button;
 public class MainActivity2 extends AppCompatActivity {
-
+    private Button btnBorrarDatos;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         ImageView image = findViewById(R.id.imageView);
-        image.setImageResource(R.drawable.reunion1);
-
+        image.setImageResource(R.drawable.reunion2);
+        btnBorrarDatos = findViewById(R.id.btn_borrar_datos);
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         Button btnSalir = findViewById(R.id.btnSalir);
         btnSalir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,7 +28,18 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        btnBorrarDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Borrar la información de SharedPreferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.remove("usuario");
+                editor.remove("password");
+                editor.apply();
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
